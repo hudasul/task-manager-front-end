0d43379ad627cf72ecb6413f65c46e946955bc18
Bint Alhuda Sultan <bntalhdysltan@gmail.com>
2025-09-03 10:36:10 +0300

    made add task functionality

--- src/components/taskComponents/TaskForm.jsx
+++ src/components/taskComponents/TaskForm.jsx
@@ -1,36 +1,90 @@
-import React from 'react'
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import axios from "axios";
 
-const TaskForm = ()=> {
+const TaskForm = ({ token }) => {
+  const navigate = useNavigate();
+  const baseUrl = import.meta.env.VITE_BACKEND_URL;
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    date: "",
+    status: "",
+    importance: "false",
+  });
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = async (event) => {
+    console.log(formData)
+    event.preventDefault();
+    await axios.post(`${baseUrl}/task/new`, formData, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    navigate("/task");
+  };
+
   return (
     <>
-    <h1>Add a New Task</h1>
-    <form >
+      <h1>Add a New Task</h1>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="title">Title:</label>
-        <input type="text" id='title' name='title'/>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          onChange={handleChange}
+          required
+        />
         <br />
         <br />
         <label htmlFor="description">Description:</label>
         <br />
-        <textarea name="description" id="description"></textarea>
+        <textarea
+          name="description"
+          id="description"
+          onChange={handleChange}
+          required
+        ></textarea>
         <br />
         <br />
         <label htmlFor="date">Deadline: </label>
-        <input type="date" name="date" id="date" />
+        <input
+          type="date"
+          name="date"
+          id="date"
+          onChange={handleChange}
+          required
+        />
         <br />
         <br />
         <label htmlFor="status">Status</label>
-        <select name="status" id="status">
-            <option value="">Pending</option>
-            <option value="">In Progress</option>
-            <option value="">Complete</option>
+        <select name="status" id="status" onChange={handleChange} required>
+          <option value="Pending">Pending</option>
+          <option value="In Progress">In Progress</option>
+          <option value="Complete">Complete</option>
         </select>
 
         <br />
         <br />
+        <label htmlFor="importance">Important? </label>
+        <input
+          type="checkbox"
+          name="importance"
+          id="importance"
+          value='true'
+          onChange={handleChange}
+        />
+        <br />
+        <br />
         <button>Add</button>
-    </form>
+      </form>
     </>
-  )
-}
+  );
+};
 
-export default TaskForm
+export default TaskForm;
